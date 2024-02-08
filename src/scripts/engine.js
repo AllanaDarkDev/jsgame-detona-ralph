a4ca76164235a51a7fc89a31dfc0640768ce107d
AllanaDarkDev <brallanadarksilvamoura@gmail.com>
2024-02-08 06:52:27 -0300

    implementado actions

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,12 +6,27 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
-        gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        countDownTimerId: setInterval(countDown, 1000),
+        timerId: setInterval(randomSquare, 1000),
     },
 };
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId)
+        clearInterval(state.actions.timerId)
+        alert("GAME OVER!")
+        alert(`resultados: ${state.values.result}`)
+    }
+}
 
 function randomSquare() {
     state.view.squares.forEach((square) => {
@@ -21,10 +36,6 @@ function randomSquare() {
     let randomSquare = state.view.squares[randomNumer];
     randomSquare.classList.add("enemy");
     state.values.hitPosition = randomSquare.id;
-}
-
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 }
 
 function addListenerHitBox() {
@@ -42,7 +53,6 @@ function addListenerHitBox() {
 }
 
 function initialize(){
-    moveEnemy();
     addListenerHitBox();
 }
 
